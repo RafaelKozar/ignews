c6d2a7932de7f585600afd0a56d20fc1cc1dbefc
RafaelKozar <rafael_kozar@hotmail.com>
2021-04-13 10:57:10 -0300

    Font Roboto + _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import {AppProps} from 'next/app'
 
+///Este é toda vez recarregado toda vez que o usuário troca de tela
 function MyApp({ Component, pageProps } : AppProps) {
   return <Component {...pageProps} />
 }
