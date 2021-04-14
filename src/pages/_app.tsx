b84913d554c5ee8b28020c90a4cbe7a1cb9910c3
RafaelKozar <rafael_kozar@hotmail.com>
2021-04-14 12:05:04 -0300

    authentication with next-auth and github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import { AppProps } from 'next/app'
 import React from 'react'
 import { Header } from '../components/Header'
+import {Provider as NextAuthProvider} from 'next-auth/client'
 import '../styles/global.scss'
 
 ///Este é toda vez recarregado toda vez que o usuário troca de tela
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
