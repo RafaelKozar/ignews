0d35fefefa6ee876f64e0157ac7653380c018286
RafaelKozar <rafael_kozar@hotmail.com>
2021-04-28 11:49:11 -0300

    receive event subscription part 2

--- src/services/stripe-js.ts
+++ src/services/stripe-js.ts
@@ -1,8 +1,7 @@
 import {loadStripe} from '@stripe/stripe-js'
 
-export async function getStripeJs() {
-    // const stripeJs = await loadStripe(process.env.NEXT_PULIC_STRIPE_PUBLIC_KEY)
-    const stripeJs = await loadStripe('pk_test_51IfsCmGar3In4DYS2idC3yKBZDWag0rixkSEub9RU0Fc0uFfvs3EsC85XEzu1q8H0bf5XJJMauGZ1G7ZnQcCjGDG00O0VmE8AK')
+export async function getStripeJs() {        
+    const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)   
 
     return stripeJs;
 }
